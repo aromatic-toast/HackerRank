ba92b82d19f71d3650ef3a2215ab237fd0773dcb
Lesley <iamthelm@gmail.com>
2020-05-02 18:00:02 -0700

    finished the countingValleys challenge

---
+++ countingValleys.py
@@ -0,0 +1,37 @@
+# Problem statement :
+# https://www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup&h_r=next-challenge&h_v=zen
+
+def countingValleys(n, s):
+    """
+    Produce the number of valleys traversed.
+    Parameters
+    ----------
+    n : (int) The number of steps in the hike.
+    s : (string) A string of length n, describing the steps in Gary's path.
+    Returns
+    -------
+    int : Returns an integer representing the number of valleys traversed in s.
+
+    Examples
+    --------
+    >>> countingValleys(n=8, s="UDDDUDUU")
+    >>> 1
+    """
+    # keep track of altitude position on hike
+    altitude = 0
+    valley_count = 0
+    for step in range(n):
+
+        # check if returning to sea-level from a valley
+        if altitude < 0 and s[step] == "U" and altitude + 1 == 0:
+            valley_count += 1
+
+        # update the altitude position
+        if s[step] == "U":
+            altitude += 1
+        if s[step] == "D":
+            altitude -= 1
+
+    return valley_count
+
+assert countingValleys(n=8, s="UDDDUDUU") == 1
